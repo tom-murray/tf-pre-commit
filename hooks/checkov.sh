24af5e3d9b845428bcb62cb7715a4eef21de10bc
Tom Murray <tom@tommurray.solutions>
2022-03-17 07:53:30 +0000

    amended checkov script

--- hooks/checkov.sh
+++ hooks/checkov.sh
@@ -4,6 +4,11 @@ set -e
 
 export PATH=$PATH:/usr/local/bin
 
-checkov -d .
+for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
+  echo "--> Running 'terraform validate' in directory '$dir'"
+  pushd "$dir" >/dev/null
+  checkov -d .
+  popd >/dev/null
+done
 
-exit ${VALIDATE_ERROR}
+exit
